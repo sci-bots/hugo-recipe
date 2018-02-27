989bb229aff0c9a209cff02b7d8406d02c6c7da3
Christian Fobel <christian@fobel.net>
2018-02-27 18:44:08 -0500

    fix(bat): fix xcopy command args

--- bld.bat
+++ bld.bat
@@ -10,8 +10,8 @@ REM Extract files from from zip archive.
 if errorlevel 1 exit 1
 
 REM Copy full archive contents to Conda environment.
-set INSTALL_PATH=%PREFIX%/library/opt/%PKG_NAME%
-xcopy /S /Y /I /Q %PKG_NAME% %INSTALL_PATH%
+set INSTALL_PATH=%PREFIX%\library\opt\%PKG_NAME%
+xcopy /S /Y /I /Q "%PKG_NAME%" "%INSTALL_PATH%"
 
 REM Make link to main executable in `Scripts` directory.
 set EXE_PATH=%CONDA_PREFIX%/library/opt/%PKG_NAME%/%PKG_NAME%.exe
