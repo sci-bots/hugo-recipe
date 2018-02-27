86d31ab4aeaf0d39489e3364c9bdee30848d3a5a
Christian Fobel <christian@fobel.net>
2018-02-27 18:51:00 -0500

    refactor(meta): simplify meta.yaml, add test

--- bld.bat
+++ bld.bat
@@ -1,17 +1,6 @@
-set PKG_URL=https://github.com/gohugoio/hugo/releases/download/v0.31.1/hugo_0.31.1_Windows-32bit.zip
-set FILENAME=hugo_0.31.1_Windows-32bit.zip
-REM Download Windows binary.
-"%PYTHON%" -m wget "%PKG_URL%"
-if errorlevel 1 exit 1
-mkdir "%PKG_NAME%"
-REM Extract files from from zip archive.
-
-"%PREFIX%"\Library\bin\7za x %FILENAME% -y -o"%PKG_NAME%"
-if errorlevel 1 exit 1
-
 REM Copy full archive contents to Conda environment.
 set INSTALL_PATH=%PREFIX%\library\opt\%PKG_NAME%
-xcopy /S /Y /I /Q "%PKG_NAME%" "%INSTALL_PATH%"
+xcopy /S /Y /I /Q "%SRC_DIR%" "%INSTALL_PATH%"
 
 REM Make link to main executable in `Scripts` directory.
 set EXE_PATH=%CONDA_PREFIX%/library/opt/%PKG_NAME%/%PKG_NAME%.exe
